6082dee94e77d9ab961c90267f158b8769a7c592
pacochas <pacochas@msoe.edu>
2025-10-27 11:24:14 -0500

    Forgot to update vector.h

--- vector.h
+++ vector.h
@@ -1,7 +1,7 @@
 /*
 * Filename: vector.h
 * Author: Sebastian Pacocha
-* Date: 10/01/2025
+* Date: 10/15/2025
 * Description: Header file for a vector struct and its associated functions.
 */
 
@@ -20,8 +20,10 @@ typedef struct {
 void add (Vector *v1, const Vector *v2);
 void subtract (Vector *v1, const Vector *v2);
 void smult (Vector *v1, double scalar);
-void list (Vector *vectors[], int count);
-void clear(Vector *vectors[], int count); // clear the vector array
+void list (Vector **vectors, int count);
+void clear(Vector **vectors, int count); // clear the vector array
+void load(const char *filename, Vector **vectors, int *vector_count); // Load vectors from given file
+void save(const char *filename, Vector **vectors, int vector_count); // Save vectors to a file
 double dot (const Vector *v1, const Vector *v2); 
 Vector cross (const Vector *v1, const Vector *v2); 
 Vector *create_or_get_vector(Vector *vectors[], int *count, char name);
@@ -29,3 +31,4 @@ Vector *find_vector(Vector *vectors[], int count, char name);
 
 
 #endif // VECTOR_H
+
